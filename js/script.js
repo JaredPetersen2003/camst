b40e6a3940bf94e649fc693e2b129754ebf3bb5c
Jared Petersen <jared.petersen9@gmail.com>
2021-04-03 11:05:25 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})
+
+$(document).ready(function() {
+    $(".dropdown-toggle").dropdown();
+});
