25848c7381072de03e496da112420c854d93bf83
Jared Petersen <jared.petersen9@gmail.com>
2021-04-18 10:26:33 +0200

    added scrollreveal, vector logo, fixed mobile nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
-var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
-var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
-  return new bootstrap.Dropdown(dropdownToggleEl)
+window.onload = (function ScrllReveal() {
+  ScrollReveal().reveal('.card', {
+    duration   : 600,
+    distance   : '20px',
+    easing     : 'ease-out',
+    origin     : 'bottom',
+    reset      : true,
+    scale      : 1,
+    viewFactor : 0,
+    afterReveal  : revealChildren
+  }, 150);
+
+  ScrollReveal().reveal('.services', {
+    duration   : 600,
+    reset: true,
+    delay: 150
+  });
+
+  
+  
+    var revealChildren = sr.reveal('.card-body, .card-text', {
+    duration   : 500,
+    scale      : 1,
+    distance   : '20px',
+    origin     : 'bottom',
+    reset      : true,
+    easing     : 'ease-out',
+    viewFactor : 1,
+  }, 75);
 })
+
+
 
 $(document).ready(function() {
     $(".dropdown-toggle").dropdown();
 });
+
